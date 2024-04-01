61642227fdfe0adf46265d5017f3287f22caf5b1
nLaykon <nLaykon@gmail.com>
2024-04-01 14:28:05 +0100

    dev

---
+++ src/main/java/org/laykon/survivaloverhaul/SurvivalOverhaul.java
@@ -0,0 +1,17 @@
+package org.laykon.survivaloverhaul;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalOverhaul extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
